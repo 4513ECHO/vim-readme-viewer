b4e8268192d0ba040879e58c88cfd92b283f239c
4513ECHO <4513echo@gmail.com>
2022-01-21 21:30:29 +0900

    🐛 fix: Fix ddu types

--- denops/@ddu-kinds/readme_viewer.ts
+++ denops/@ddu-kinds/readme_viewer.ts
@@ -1,21 +1,29 @@
-import { BaseKind } from "https://pax.deno.dev/Shougo/ddu.vim/denops/ddu/types.ts";
+import type { ActionArguments } from "https://pax.deno.dev/Shougo/ddu.vim@492abe0/denops/ddu/base/kind.ts";
 import type { Denops } from "https://deno.land/x/denops_std@v1.11.3/mod.ts";
-import type { ActionArguments } from "https://pax.deno.dev/Shougo/ddu.vim/denops/ddu/base/kind.ts";
+import {
+  ActionFlags,
+  BaseKind,
+} from "https://pax.deno.dev/Shougo/ddu.vim@492abe0/denops/ddu/types.ts";
 
-interface ActionData {
+export interface ActionData {
   name: string;
 }
 
 type Params = Record<never, never>;
 
-export class Kind extends BaseKind<Params, ActionData> {
-  actions: Record<string, (args: ActionArguments<Params>) => Promise<void>> = {
+export class Kind extends BaseKind<Params> {
+  actions: Record<
+    string,
+    (args: ActionArguments<Params>) => Promise<ActionFlags>
+  > = {
     open: async (args) => {
       for (const item of args.items) {
         const action = item?.action as ActionData;
         const name = action.name;
         await args.denops.call("readme_viewer#open", name, "");
       }
+
+      return Promise.resolve(ActionFlags.None);
     },
   };
 
--- denops/@ddu-sources/readme_viewer.ts
+++ denops/@ddu-sources/readme_viewer.ts
@@ -1,12 +1,9 @@
-import { BaseSource } from "https://pax.deno.dev/Shougo/ddu.vim/denops/ddu/types.ts";
-import type { Item } from "https://pax.deno.dev/Shougo/ddu.vim/denops/ddu/types.ts";
-import type { GatherArguments } from "https://pax.deno.dev/Shougo/ddu.vim/denops/ddu/base/source.ts";
 import type { Denops } from "https://deno.land/x/denops_std@v1.11.3/mod.ts";
+import type { GatherArguments } from "https://pax.deno.dev/Shougo/ddu.vim@492abe0/denops/ddu/base/source.ts";
+import type { Item } from "https://pax.deno.dev/Shougo/ddu.vim@492abe0/denops/ddu/types.ts";
+import { BaseSource } from "https://pax.deno.dev/Shougo/ddu.vim@492abe0/denops/ddu/types.ts";
 import { ensureObject } from "https://deno.land/x/unknownutil@v0.1.1/mod.ts";
-
-interface ActionData {
-  name: string;
-}
+import type { ActionData } from "../@ddu-kinds/readme_viewer.ts";
 
 type Params = Record<never, never>;
 
@@ -16,25 +13,22 @@ interface PluginData {
 }
 
 export class Source extends BaseSource<Params, ActionData> {
-  kind = "readme";
+  kind = "readme_viewer";
 
   gather(args: GatherArguments<Params>): ReadableStream<Item<ActionData>[]> {
     return new ReadableStream({
       async start(controller) {
-        const list = async (): Promise<Item<ActionData>[]> => {
-          const plugins = await args.denops.call("readme_viewer#get");
-          ensureObject<PluginData>(plugins);
-          return Object.values(plugins).map((i) => {
+        const plugins = await args.denops.call("readme_viewer#get");
+        ensureObject<PluginData>(plugins);
+        controller.enqueue(
+          Object.values(plugins).map((i) => {
             return {
               word: i.name,
               action: {
                 name: i.name,
               },
-            };
-          });
-        };
-        controller.enqueue(
-          await list(),
+            } as Item<ActionData>;
+          }),
         );
         controller.close();
       },
